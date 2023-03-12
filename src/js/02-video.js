aeebd3d568dc844eb9374494ad8ac64e6911c8af
Oleksii Leschenko <djlabuh@gmail.com>
2023-03-12 13:12:12 +0200

    hw-2-videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+function saveCurrentTime() {
+  player.getCurrentTime()
+    .then(function(seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function loadCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+  if (currentTime) {
+    player.setCurrentTime(currentTime)
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+}
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+loadCurrentTime();
